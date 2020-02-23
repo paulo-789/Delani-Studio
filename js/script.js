2cfa75c46e76dca3408eb753a7014829919eaa4c
Paul Onyango <jorhnepaulone@gmail.com>
2020-02-23 19:27:15 +0300

    styled images in the portfolio to hover

--- js/script.js
+++ js/script.js
@@ -23,16 +23,63 @@ $(".bbb").click(function(){
     $(this).hide();
     $("#aaa").show();
 })
-
+$("#mg1").hover(function(){
+    $(".overlay1").show();
+})
+$("#mg1").mouseleave(function(){
+    $(".overlay1").hide(); 
+});
+$("#mg2").hover(function(){
+    $(".overlay2").show();
+});
+$("#mg2").mouseleave(function(){
+    $(".overlay2").hide();
+})
+$("#mg3").hover(function(){
+    $(".overlay3").show();
+})
+$("#mg3").mouseleave(function(){
+    $(".overlay3").hide();
+}) 
+$("#mg4").hover(function(){
+    $(".overlay4").show();
+})
+$("#mg4").mouseleave(function(){
+    $(".overlay4").hide(); 
+});
+$("#mg5").hover(function(){
+    $(".overlay5").show();
+})
+$("#mg5").mouseleave(function(){
+    $(".overlay5").hide(); 
+});
+$("#mg6").hover(function(){
+    $(".overlay6").show();
+})
+$("#mg6").mouseleave(function(){
+    $(".overlay6").hide(); 
+});
+$("#mg7").hover(function(){
+    $(".overlay7").show();
+})
+$("#mg7").mouseleave(function(){
+    $(".overlay7").hide(); 
+});
+$("#mg8").hover(function(){
+    $(".overlay8").show();
+})
+$("#mg8").mouseleave(function(){
+    $(".overlay8").hide(); 
+});
 $("#submit").click(function(){
   var Name=$("#name").val();
   var Email=$("#email").val();
   var Text=$("#message").val();
 if (Name==""&&Email==""&&Text==""){
-  alert("please fill the form corect details")
+  alert("please go back and input the correct credentials!")
 }
   else{
-   alert("hello" + Name + "we  have recived your massage")
+   alert( "hello"  +  Name  +  "We have received your message.Thank you for reaching out to us")
   }
 })
 })
